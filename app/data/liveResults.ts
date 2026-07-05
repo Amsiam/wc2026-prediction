3ac0bded057abfb18f60ecda5179494c81960bf3
Amsiam <55598241+Amsiam@users.noreply.github.com>
2026-07-05 17:21:06 +0000

    chore: sync WC 2026 match results

--- app/data/liveResults.ts
+++ app/data/liveResults.ts
@@ -1347,8 +1347,16 @@ export const LIVE_RESULTS = {
   "ALG": -1,
   "ARG": -2,
   "AUT": -4,
-  "JOR": -4
+  "JOR": -4,
+  "COL": -4,
+  "POR": -4,
+  "UZB": -4,
+  "COD": -5,
+  "ENG": -2,
+  "CRO": -2,
+  "GHA": -3,
+  "PAN": -5
 } as Record<string, number>,
-  syncedAt: "2026-07-05T13:57:59.073Z",
+  syncedAt: "2026-07-05T17:21:06.163Z",
   source: "openfootball/worldcup.json + wikipedia",
 }
